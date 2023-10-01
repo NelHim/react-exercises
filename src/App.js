9dc6e4b0fa2101d17a02059d5b5d4cbc58ce8bc1
NelHim <n.himbaza@alustudent.com>
2023-10-01 19:33:48 +0200

    Building a form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 function App() {
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: ""
+  });
+
+  function handleChange (event) {
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value
+      }
+    })
+  }
+
+  function handleSubmit (event){
+    event.preventDefault()
+    alert(`Hello ${formData.firstName} ${formData.lastName}!`)
+  }
+
+
   return (
-    <div>
-      <h1 className="text-2xl text-blue-500 font-bold text-center">React Exercises</h1>
+    <div className="grid justify-center items-center h-screen">
+      <form onSubmit={handleSubmit} className="grid gap-5">
+        <input type="text" className="border-solid border-black rounded-md border-2 py-2 px-2" placeholder="First Name" onChange={handleChange} name="firstName" value={formData.firstName}/>
+        <input type="text" className="border-solid border-black rounded-md border-2 py-2 px-2"  placeholder="Last Name" onChange={handleChange} name="lastName" value={formData.lastName}/> 
+        <button className="bg-red-600 text-white font-bold px-8 py-2 rounded-md">GREET ME</button>
+      </form>
     </div>
   );
 }
