783ac8eae7d91b226b436128ea342d74ac53d2f4
NelHim <n.himbaza@alustudent.com>
2023-10-01 22:01:54 +0200

    Fetching data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
+import Card from "./components/Card";
+
 function App() {
+
+  const [users, setUsers] = useState({})
+  const [changeable, setChangeable] = useState(true)
+  // let changeable = true;
+
+  useEffect(function() {
+    fetch("https://random-data-api.com/api/users/random_user?size=10")
+        .then(res => res.json())
+        .then(data => setUsers(data))
+}, [changeable])
+
+console.log(users)
+
+
+
   return (
-    <div>
-      <h1 className="text-2xl text-blue-500 font-bold text-center">React Exercises</h1>
+    <div className="grid justify-center items-center h-screen">
+      <button onClick={() => setChangeable(prevChangeable => !prevChangeable)} className="bg-red-500 text-white text-xl font-bold py-2 px-4 border rounded-md">Fetch Random</button>
+
     </div>
   );
 }
