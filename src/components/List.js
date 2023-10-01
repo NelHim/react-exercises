70ecdec211361ee7a34d630ec01ebccafb58fe7a
NelHim <n.himbaza@alustudent.com>
2023-10-01 18:24:24 +0200

    Create modifications

--- src/components/List.js
+++ src/components/List.js
@@ -3,17 +3,17 @@ import React from 'react'
 const List = ({items}) => {
     let listArray = items.map((item) => {
         if (item === 'dog'){
-            return <li key={item}>{`🐶 ${item}`}</li>
+            return <li className='border border-gray-400 py-4 px-6' key={item}>{`🐶 ${item}`}</li>
         }else if(item === 'cat'){
-            return <li key={item}>{`😸 ${item}`}</li>
+            return <li className='border border-gray-400 py-4 px-6' key={item}>{`😸 ${item}`}</li>
         }else if (item === 'chicken'){
-            return <li key={item}>{`🐔 ${item}`}</li>
+            return <li className='border border-gray-400 py-4 px-6' key={item}>{`🐔 ${item}`}</li>
         }else if (item === 'cow'){
-            return <li key={item}>{`🐮 ${item}`}</li>
+            return <li className='border border-gray-400 py-4 px-6' key={item}>{`🐮 ${item}`}</li>
         }else if (item === 'sheep'){
-            return <li key={item}>{`🐏 ${item}`}</li>
+            return <li className='border border-gray-400 py-4 px-6' key={item}>{`🐏 ${item}`}</li>
         }else if (item === 'horse'){
-            return <li key={item}>{`🐎 ${item}`}</li>
+            return <li className='border border-gray-400 py-4 px-6' key={item}>{`🐎 ${item}`}</li>
         }
     })
   return (
