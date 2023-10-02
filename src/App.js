d6705e1ac8565a331dd747cb0ee3bb991e9830b7
NelHim <n.himbaza@alustudent.com>
2023-10-02 14:36:17 +0200

    Create calculator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl text-blue-500 font-bold text-center">React Exercises</h1>
+    <div className="grid h-screen justify-center items-center">
+      <div className="grid grid-cols-4 grid-rows-6 border">
+          <p className="col-span-4">Nelson</p>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">+/-</button>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">A/C</button>
+          <button className="border p-8">A/C</button>
+      </div>
     </div>
   );
 }
