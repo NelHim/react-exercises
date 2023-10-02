891e7d9f0adc5e4fc104278b524a90c40d4fd863
NelHim <n.himbaza@alustudent.com>
2023-10-02 11:58:58 +0200

    Working with an API

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import Card from "./components/Card";
 
 function App() {
 
-  const [users, setUsers] = useState({})
+  const [users, setUsers] = useState([])
   const [changeable, setChangeable] = useState(true)
-  // let changeable = true;
 
   useEffect(function() {
     fetch("https://random-data-api.com/api/users/random_user?size=10")
@@ -13,14 +12,18 @@ function App() {
         .then(data => setUsers(data))
 }, [changeable])
 
-console.log(users)
+console.log(users)    
 
-
+  let elements = users.map(user => {
+    return <Card {...user} /> 
+  })
 
   return (
-    <div className="grid justify-center items-center h-screen">
+    <div className="grid justify-center gap-4">
       <button onClick={() => setChangeable(prevChangeable => !prevChangeable)} className="bg-red-500 text-white text-xl font-bold py-2 px-4 border rounded-md">Fetch Random</button>
-
+      <div className="grid grid-cols-2 bg-[#E1F6DC] p-4 gap-8">
+        {elements}
+      </div>
     </div>
   );
 }
